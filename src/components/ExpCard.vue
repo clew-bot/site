4bd79ad73ab1c7ae44a7897d753bb5180733cdc3
Chad Lew <lewchad11@gmail.com>
2023-03-24 13:50:41 -0700

    change up layout

--- src/components/ExpCard.vue
+++ src/components/ExpCard.vue
@@ -21,14 +21,6 @@ const themeStore = useThemeStore();
       <h3 :class="themeStore.theme+'descriptions'"><i>Portfolio worthy applications</i><span>&nbsp;🙌</span></h3>
       </div>  
       <div class="proj-container">
-        <div :class="`${themeStore.theme}proj-bg`">
-          <img :src="purplev3" alt="purplev3" class="proj-image">
-          <h1 :class="themeStore.theme+'proj-title'">Purple v3</h1>
-          <p class="proj-desc altine">Purple v3 is a MMORPG Discord bot game with multiple functionalities. Users can grind skills by hunting, fishing, chopping, entering the dungeon, compete in fishing contests, and much more. Uses DiscordJS library and MongoDB for the database.</p>
-          <p class="made-with">Made with Javascript </p>
-          <a class="linkz" target="_blank" href="https://github.com/clew-bot/Purple_v3"><img :src="Octocat" alt="" class="octo"></a>
-       
-        </div>
         <div :class="themeStore.theme+'proj-bg'">
           <img :src="altine" alt="altine" class="proj-image">
           <h1 :class="themeStore.theme+'proj-title'">ASM</h1>
@@ -37,6 +29,15 @@ const themeStore = useThemeStore();
           <a class="linkz" target="_blank" href="https://github.com/clew-bot/asm"><img :src="Octocat" alt="" class="octo"></a>
        
         </div>
+        <div :class="`${themeStore.theme}proj-bg`">
+          <img :src="purplev3" alt="purplev3" class="proj-image">
+          <h1 :class="themeStore.theme+'proj-title'">Purple v3</h1>
+          <p class="proj-desc altine">Purple v3 is a MMORPG Discord bot game with multiple functionalities. Users can grind skills by hunting, fishing, chopping, entering the dungeon, compete in fishing contests, and much more. Uses DiscordJS library and MongoDB for the database.</p>
+          <p class="made-with">Made with Javascript </p>
+          <a class="linkz" target="_blank" href="https://github.com/clew-bot/Purple_v3"><img :src="Octocat" alt="" class="octo"></a>
+       
+        </div>
+  
         <div :class="themeStore.theme+'proj-bg'">
           <img :src="altitude" alt="altitude" class="proj-image">
           <h1 :class="themeStore.theme+'proj-title'">altitude</h1>
